6023db838e3e9d36d9578d771aaec6de1c768dd1
DigiCoders Technologies <digicodersdevelopment@gmail.com>
2026-05-09 20:02:05 +0530

    first commit

---
+++ code1.js
@@ -0,0 +1,18 @@
+// const add =(a,b)=>{
+//     return a+b 
+// }
+// const subract = (a,b)=>{
+//     return a-b
+// }
+// const multi=(a,b)=>{
+//     return a*b
+// }
+// module.exports={add,subract}
+
+
+export const add = (a,b)=>{
+    return a+b
+}
+export const subract = (a,b)=>{
+    return a-b
+}
---
+++ code2.js
@@ -0,0 +1,9 @@
+// const {add,subract}= require('./code1')
+
+// console.log(add(10,20));
+// console.log(subract(30,20));
+
+
+import {add,subract} from './code1.js'
+console.log(add(10,20));
+console.log(subract(100,20));
